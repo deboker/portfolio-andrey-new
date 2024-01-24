044814fd02619452cd4b536f2d081528e3c6b33b
deboker <117391017+deboker@users.noreply.github.com>
2024-01-24 13:39:51 +0100

    hhh

--- vite.config.js
+++ vite.config.js
@@ -1,4 +1,3 @@
-// vite.config.js
 import { defineConfig } from 'vite';
 
 export default defineConfig({
@@ -6,12 +5,10 @@ export default defineConfig({
     outDir: 'dist',
     assetsDir: 'assets', // Output directory for assets, relative to outDir
     assetsInlineLimit: 4096,
-    assetsInclude: ['**/*.pdf'], // Include PDF files in the build process
     rollupOptions: {
       output: {
         manualChunks: undefined,
       },
     },
   },
-  publicDir: 'images', // Set the public directory to point to the "images" folder
 });
